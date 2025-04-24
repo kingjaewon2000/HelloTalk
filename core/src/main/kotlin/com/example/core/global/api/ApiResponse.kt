59fb6a0b7494e6ca1c5f82cb8c0770eccfa82390
kingjaewon2000 <kkkxx_00@naver.com>
2025-04-24 12:37:49 +0900

    feat: 공통 응답 형식 개발

---
+++ core/src/main/kotlin/com/example/core/global/api/ApiResponse.kt
@@ -0,0 +1,11 @@
+package com.example.core.global.api
+
+/*
+ * API 응답 상태와 데이터를 감싸는 제네릭 클래스
+ */
+sealed class ApiResponse<out T> {
+
+    data class Success<T>(val status: Int, val data: T) : ApiResponse<T>()
+
+    data class Error(val error: RuntimeException) : ApiResponse<Nothing>()
+}
